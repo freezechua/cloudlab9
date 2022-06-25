2e5e7f09b557bf21e3b6bda835c9f456b42fd9f6
freezechua <100996953+freezechua@users.noreply.github.com>
2022-06-25 16:36:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>RedStore</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>RedStore</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript"></script>
+	<script src="js/reg.js"></script>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="navbar">
+				<div class="logo">
+					<img src="images\logo.png" width="125px">
+				</div>
+				<nav>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="product.php">Products</a></li>
+						<li><a href="cart.php">Cart</a></li>
+						<li><a href="account.php">Account</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="row">
+				<div class="col-2">
+					<h1>Give your Workout<br>A New Style!</h1>
+					<p>Success isn't always about greatness. It's about consistency. Consistent<br>hard work gains success. Greatness will come.</p>
+					<a href="product.php" class="btn">Explore Now &#8594;</a>
+				</div>
+				<div class="col-2">
+					<img src="images\image1.png">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!------ featured products------>
+
+	<div class="categories">
+		<div class="small-container">
+			<h2 class="title">Products</h2>
+			<div class="row">
+				<div class="col-4">
+					<img src="images\product-1.jpg">
+					<h4>Red T-shirt</h4>
+					<p>RM50.00</p>
+				</div>
+				<div class="col-4">
+					<img src="images\product-2.jpg">
+					<h4>Black Shoes</h4>
+					<p>RM80.00</p>
+				</div>
+				<div class="col-4">
+					<img src="images\product-3.jpg">
+					<h4>Classic Pants</h4>
+					<p>RM40.00</p>
+				</div>
+				<div class="col-4">
+					<img src="images\product-8.jpg">
+					<h4>Fossil Watch</h4>
+					<p>RM100.00</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+
+</html>
